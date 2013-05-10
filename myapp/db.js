f144f7194011f284df6ddcaf3607803fd32c0ee7
Illusioneer <khillton@gmail.com>
2013-05-10 16:05:51 -0400

    Express DB

---
+++ myapp/db.js
@@ -0,0 +1,15 @@
+var moment = require('moment');
+var pg = require('pg');
+var client = new pg.Client('postgres://master1:harper123@localhost:5432/mastercontrol');
+client.connect();
+
+exports.updatestats = function() {
+        console.time('sqlperftest')
+        var starttime = moment().subtract('minutes', 5).format('YYYY-MM-DD h:mm');
+        var nowtime =  moment().format('YYYY-MM-DD h:mm');
+        var thesql = "SELECT * FROM servicestatuses WHERE nagiostimeid >= '" + starttime + "' AND nagiostimeid < '" + nowtime + "' AND current_state >= 1 AND current_state < 8"
+        var query = client.query(thesql);
+        query.on('row', function(row) {
+            return JSON.parse("{" + row.servicedata.replace(/\=\>/g,":").replace(/NULL/g,'"NULL"') + "}");
+        });
+    }
