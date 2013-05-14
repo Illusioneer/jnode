0f6fcfd8a938d850da75cd51a9004ef0294fb439
Illusioneer <khillton@gmail.com>
2013-05-14 14:10:23 -0400

    Callback test 3

--- myapp/db.js
+++ myapp/db.js
@@ -1,17 +1,18 @@
 exports.updatestats = function(minutes, attribs, callback) {
-        var moment = require('moment');
-        var pg = require('pg');
-        var client = new pg.Client('postgres://master1:harper123@localhost:5432/mastercontrol');
-        client.connect();
-        var starttime = moment().subtract('minutes', minutes).format('YYYY-MM-DD h:mm');
-        var nowtime =  moment().format('YYYY-MM-DD h:mm');
-        var thesql = "SELECT * FROM servicestatuses WHERE nagiostimeid >= '" + starttime + "' AND nagiostimeid < '" + nowtime + "' AND current_state >= 1 AND current_state < 8";
-        rowset = [];
 
-        var query = client.query(thesql);
-
-        query.on('row', function(row) {rowset.push(JSON.parse("{" + row.servicedata.replace(/\=\>/g,":").replace(/NULL/g,'"NULL"') + "}"));});
-        attribs.serv = rowset;
-        callback('node', attribs);
+    var moment = require('moment');
+    var pg = require('pg');
+    var starttime = moment().subtract('minutes', minutes).format('YYYY-MM-DD h:mm');
+    var nowtime =  moment().format('YYYY-MM-DD h:mm');
+    var conString = "postgres://master1:harper123@localhost:5432/mastercontrol";
+    var sqlquery = "SELECT * FROM servicestatuses WHERE nagiostimeid >= '" + starttime + "' AND nagiostimeid < '" + nowtime + "' AND current_state >= 1 AND current_state < 8";
+    pg.connect(conString, function(err, client) {
+        client.query(sqlquery, function(err, result) {
+            console.log("Row count: %d",result.rows.length);  // 1
+            console.log("First one is:", result.rows[0]);
+            attribs.data = result.rows;
+            callback("node",attribs);
+        });
+    });
     }
 
