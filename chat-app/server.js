2b83f5c5ce6a336bac7db3bc5e5c08769c212b7f
Illusioneer <khillton@gmail.com>
2013-05-21 10:35:06 -0400

    New entries

--- chat-app/server.js
+++ chat-app/server.js
@@ -39,7 +39,7 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('entryping', function(data){
         console.log("User: " + data.uid + " has deleted ID " + data.statid);
-        socket.broadcast.to(data.chat).emit("pong",{uid: data.uid, statid: data.statid});
+        socket.broadcast.to(data.chat).emit("entrypong",{uid: data.uid, statid: data.statid});
     });
 
     socket.on('updatestats', function (data) {
