0fda8c4687af2d6658e422df20b7d4c66a98a96c
Illusioneer <khillton@gmail.com>
2013-06-02 00:30:57 -0400

    Pushed from kdevelop

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var moment = require('moment');
 var pg = require('pg');
 var client = new pg.Client('postgres://master1:harper123@localhost:5432/mastercontrol');
 client.connect();
-
+//added this on a whim
 
 app.configure(function(){
     app.use(express.static(__dirname + '/'));
