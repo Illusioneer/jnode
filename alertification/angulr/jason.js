e42107a5165262281b9ee4314eea525233a8d254
Illusioneer <khillton@gmail.com>
2013-07-10 11:55:09 -0400

    Added Monitor express app

--- alertification/angulr/jason.js
+++ alertification/angulr/jason.js
@@ -1,30 +1,15 @@
-angular.module('Graphite', ['ngResource']).config(['$httpProvider', function($httpProvider) {
-    delete $httpProvider.defaults.headers.common["X-Requested-With"]
-}]);
+angular.module('Graphite', ['ngResource']).controller('GraphiteCtrl', function ($scope, $http) {
+    var url = 'http://graphite.hcpprod.com/render?format=json&target=stats.gauges.WEBP02.cpu.1.Avg&from=-10min';
+    $scope.doSearch = function() {
+        $http.get(url)
+            .success(function(data) {
+                console.log(data[0].datapoints);
+                $scope.graphiteResult = data[0];
+                $scope.daFunk = "We got da funk!";
+            });
+    };
+});
 
-function GraphiteCtrl($scope, $resource) {
-    $scope.graphite = $resource('http://mcp.hcpprod.com:3000/posts?host_name=localhost');
-
-$scope.doSearch = function () {
-    $scope.graphiteResult = $scope.graphite.get();
-};
-}
-
-define('DB_NAME', 'hc_genie');
-
-/** MySQL database username */
-define('DB_USER', 'hc_genie_user');
-
-/** MySQL database password */
-define('DB_PASSWORD', '1tzM@jickZ!');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-define('WP_DEBUG', false);
+$(function(){
+    $("select").multiselect();
+});
